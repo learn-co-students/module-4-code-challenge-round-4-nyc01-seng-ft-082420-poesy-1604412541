9ba0fa652ff70bc1e1c249ba0cc7d5e072802e7e
jessicasalbert <jessicarsalbert@gmail.com>
2020-11-03 09:58:59 -0500

    completed core deliverables

--- src/App.js
+++ src/App.js
@@ -6,21 +6,44 @@ import NewPoemForm from "./NewPoemForm";
 class App extends React.Component {
 
   state = {
-    poems: []
+    poems: [],
+    showForm: false
+  }
+
+  toggleForm = () => {
+    this.setState((prev) => ({showForm: !prev.showForm}))
+  }
+
+  addNewPoem = (poem) => {
+    const configObj = {
+      method: "POST",
+      headers: {
+        "content-type" : "application/json",
+        accept: "application/json"
+      }, 
+      body: JSON.stringify(poem)
+    }
+    fetch(`http://localhost:6001/poems`, configObj)
+    .then(res => res.json())
+    .then(res => this.setState( prev => ({
+      poems: [...prev.poems, res]
+    })))
+    .catch(error => console.log(error.message))
   }
 
   componentDidMount() {
     fetch(`http://localhost:6001/poems`)
     .then(res => res.json())
     .then(res => this.setState(() => ({poems: res})))
+    .catch(error => console.log(error.message))
   }
 
   render() {
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick={this.toggleForm}>Show/hide new poem form</button>
+          {this.state.showForm ? <NewPoemForm submitHandler={this.addNewPoem}/> : null}
         </div>
         <PoemsContainer poems={this.state.poems}/>
       </div>
--- src/NewPoemForm.js
+++ src/NewPoemForm.js
@@ -1,12 +1,35 @@
 import React from "react";
 
 class NewPoemForm extends React.Component {
+
+  state = {
+    title: "",
+    author: "",
+    content: ""
+  }
+
+  localSubmitHandler = (e) => {
+    e.preventDefault()
+    this.props.submitHandler(this.state)
+    this.setState( () => ({
+      title: "",
+      author: "",
+      content: ""
+    }))
+  }
+
+  editForm = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  
   render() {
     return (
-      <form className="new-poem-form">
-        <input placeholder="Title" />
-        <input placeholder="Author" />
-        <textarea placeholder="Write your masterpiece here..." rows={10} />
+      <form onSubmit={this.localSubmitHandler} className="new-poem-form">
+        <input onChange={this.editForm} placeholder="Title" value={this.state.title} name="title"/>
+        <input onChange={this.editForm} placeholder="Author" value={this.state.author} name="author"/>
+        <textarea onChange={this.editForm} placeholder="Write your masterpiece here..." rows={10} value={this.state.content} name="content"/>
         <input type="submit" value="Share your masterpiece" />
       </form>
     );
