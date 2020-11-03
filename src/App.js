181807ba07b507742a1036c38e5f83c86cca6ba0
Sean LaFlam <sean.laflam@gmail.com>
2020-11-03 09:48:56 -0500

    2nd Complete

--- src/App.js
+++ src/App.js
@@ -14,13 +14,19 @@ class App extends React.Component {
     return this.state.poems
   }
 
+  formClickHandler = () => {
+    this.setState((prevState) => ({
+      showForm: !prevState.showForm
+    }))
+  }
+
   render() {
     let arrayOfPoems = this.renderPoems()
     return (
       <div className="app">
         <div className="sidebar">
-          <button>Show/hide new poem form</button>
-          {false && <NewPoemForm />}
+          <button onClick = {this.formClickHandler}>Show/hide new poem form</button>
+          {this.state.showForm ?  <NewPoemForm /> : false }
         </div>
         <PoemsContainer poems = {arrayOfPoems}/>
       </div>
