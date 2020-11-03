b2f5330f41eed2810f65cded03a776cfba17a6e4
jasonchilcott <jasonchilcottcode@gmail.com>
2020-11-03 09:52:36 -0500

    initial commit

--- src/PoemsContainer.js
+++ src/PoemsContainer.js
@@ -2,11 +2,31 @@ import React from "react";
 import Poem from "./Poem";
 
 class PoemsContainer extends React.Component {
+
+  state= {
+    poems: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:6001/poems')
+    .then (r => r.json())
+    .then (poems => this.setState({poems: poems}))
+    .catch(e => console.error(e))
+
+  }
+
+  renderPoems  = () => this.state.poems.map((poem => {
+    console.log(this.state.poems)
+
+    return <Poem key={poem.id} poem={poem}/>
+  })
+)
+
   render() {
     return (
       <div className="poems-container">
         {
-          // render poems here
+          this.renderPoems()
         }
       </div>
     );
