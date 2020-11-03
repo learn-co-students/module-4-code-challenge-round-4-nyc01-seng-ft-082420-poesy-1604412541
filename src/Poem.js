e2cd36f47ce408216ee4b5e563221420cc7df920
ashabahmed <ashabahmed7@gmail.com>
2020-11-03 10:51:49 -0500

    removed buttons for the favorited poems

--- src/Poem.js
+++ src/Poem.js
@@ -29,13 +29,13 @@ class Poem extends React.Component {
         <p>
           <strong>- By {this.props.poem.author}</strong>
         </p>
-        <button onClick={this.handleClick}>{this.state.clicked ? "Mark as read" : "Mark as unread"}</button>
+        {this.props.favorite ? null : <button onClick={this.handleClick}>{this.state.clicked ? "Mark as read" : "Mark as unread"}</button>}
         <br/>
         <br/>
-        <button onClick={this.handleFavorite}>Favorite this</button>
+        {this.props.favorite ? null : <button onClick={this.handleFavorite}>Favorite this</button>}
         <br/>
         <br/>
-        <button onClick={this.handleDelete}>Delete Poem</button>
+        {this.props.favorite ? null : <button onClick={this.handleDelete}>Delete Poem</button>}
       </div>
     );
   }
